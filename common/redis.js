2004d11ae49ce7378a978f8f0c5ece156d132623
bakajinsei <cqggff@gmail.com>
2016-12-23 11:20:27 +0800

    Add redis listener

--- common/redis.js
+++ common/redis.js
@@ -2,4 +2,11 @@
 const Redis = require('ioredis');
 const config = require('../config/');
 let redisClient = new Redis(config.redis);
+redisClient.on('error', function (err) {
+    LOG.error('redisClient error');
+    LOG.error(err);
+});
+redisClient.on('connect', function (err) {
+    LOG.info('redisClient connect');
+});
 module.exports = redisClient;
--- config/config-dev.js
+++ config/config-dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    db:'mongodb://127.0.0.1:27017/acgo'
+    db:'mongodb://127.0.0.1:27017/acgo-dev'
 }
