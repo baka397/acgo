8fda7a30907442d3228aeaaa71b58b6b1e845704
bakajinsei <cqggff@gmail.com>
2016-12-23 11:41:33 +0800

    Remove packages

--- config/index.js
+++ config/index.js
@@ -1,7 +1,6 @@
 'use strict';
 let tool = require('../common/tool');
 let pkg = require('../package.json');
-let _ = require('lodash');
 
 // 默认配置
 let defaultConfig = {
@@ -38,6 +37,6 @@ let startupConfig = process.env.CFG_PATH || ('./config-' + (process.env.NODE_ENV
 let config = {};
 try {console.log('启动配置文件：%s', startupConfig);config = require(startupConfig);} catch(e) {console.error('未找到启动配置：%s', startupConfig)};
 // 获取当前部署环境对应配置
-config = _.extend({}, defaultConfig, config || {});
+config = Object.assign({}, defaultConfig, config || {});
 
 module.exports = config;
