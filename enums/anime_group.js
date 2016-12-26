ba4c6b310a928bb6abde2a578797a1f0e7bf0eee
bakajinsei <cqggff@gmail.com>
2016-12-26 17:40:57 +0800

    修改D站item检测规则

--- enums/anime_group.js
+++ enums/anime_group.js
@@ -13,7 +13,7 @@ exports.type={
     '2':{
         'name':'D站',
         'taskRegExp':/^http:\/\/www.dilidili.com\/anime\/[a-z0-9\_\-]+\/$/i,
-        'itemRegExp':/^http:\/\/www.dilidili.com\/watch\/\d+\/$/i
+        'itemRegExp':/^http:\/\/www.dilidili.com\/watch[^\/]*\/\d+\/$/i
     },
     '3':{
         'name':'爱奇艺',
