08e81f587d0e27ecd75b6a7baea51691c7c23c44
bakajinsei <cqggff@gmail.com>
2017-01-09 14:30:02 +0800

    Update config

--- config/config-case.js
+++ config/config-case.js
@@ -1,5 +1,6 @@
 module.exports = {
     db:'mongodb://127.0.0.1:27017/acgo',
+    redisNamespace:'acgo-case',
     log: {
         path: './logs/',
         type: 'dateFileLog',
--- config/config-dev.js
+++ config/config-dev.js
@@ -1,5 +1,6 @@
 module.exports = {
     db:'mongodb://127.0.0.1:27017/acgo-dev',
+    redisNamespace:'acgo-dev',
     //管理员配置
     admins:{
         'crawler@acgo.club':'crawler',
