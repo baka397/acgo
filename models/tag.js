f18fce085771336cf0d2af7e70c805d73fc3da70
bakajinsei <cqggff@gmail.com>
2017-01-25 11:08:32 +0800

    Fixed tag valid rule

--- models/tag.js
+++ models/tag.js
@@ -7,13 +7,13 @@ const Schema = mongoose.Schema;
 let tagNameValidator = [
     validate({
         validator: 'isLength',
-        arguments: [2, 10],
+        arguments: [2, 20],
         message: '标签名称需要{ARGS[0]}-{ARGS[1]}之间的字符串'
     }),
     validate({
         validator: 'matches',
-        arguments: [/^[\u4E00-\u9FA5A-Za-z0-9\.\_]+$/],
-        message: '标签名称只能为中文,字母,数字,.,-,_'
+        arguments: [/^[\u4e00-\u9fa5\u0800-\u4e00A-Za-z0-9\.\_\'\,\-\s]+$/],
+        message: '标签名称只能为中文,字母,数字,空格或特殊字符(._\',-)'
     })
 ];
 let tagAliasValidator = [
